25b63eafd5c69fe264be17ff0de071c41ab71cd8
usaa_testing <usaa_testing@Jamess-MacBook-Pro-2.local>
2017-12-12 17:20:04 -0600

    initial server setup

---
+++ source/app.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+import Application from './components/Application.react'
+
+ReactDOM.render(<Application />, document.getElementById('test-application'));
---
+++ source/components/Application.react.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+class Application extends React.Component {
+  render() {
+    return (
+      <div className="">
+        <h1>Test</h1>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Application;
