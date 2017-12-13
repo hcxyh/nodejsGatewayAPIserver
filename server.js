473cf503ce754c0d8d6953ff1aa27c8752bfba2d
usaa_testing <usaa_testing@Jamess-MacBook-Pro-2.local>
2017-12-13 14:02:33 -0600

    lint cleanup

--- server.js
+++ server.js
@@ -1,96 +1,82 @@
 #!/bin/env node
-
-
 import express from 'express'
 import fs from 'fs'
 import path from 'path'
-import React from 'react'
-import App from './source/Components/Application.react.js'
+// import React from 'react'
+// import App from './source/Components/Application.react.js'
 
+// local host only example
+// const ipAddr = '127.0.0.1'
+// const port = '8080'
 
-//local host only example
-const ip_addr = '127.0.0.1';
-const port = '8080';
-
-const server = function() {
-
+const Server = function () {
   //  Scope.
-  const self = this;
+  const self = this
 
-
-  self.setupVariables = function() {
+  self.setupVariables = function () {
     //  Set the environment variables we need.
-    self.ipaddress = ip_addr || '127.0.0.1';
-    self.port = port || 8080;
-
-    if (typeof self.ipaddress === "undefined") {
-      console.warn('No IP var, using 127.0.0.1');
-      self.ipaddress = "127.0.0.1";
-    }
-  };
-
+    // self.ipaddress = ipAddr || '127.0.0.1'
+    // self.port = port || 8080
+    //
+    // if (typeof self.ipaddress === "undefined") {
+    //   console.warn('No IP var, using 127.0.0.1');
+    //   self.ipaddress = "127.0.0.1";
+    // }
+  }
 
   /**
    *  Populate the cache.
    */
-  self.populateCache = function() {
-    if (typeof self.zcache === "undefined") {
+  self.populateCache = function () {
+    if (typeof self.zcache === 'undefined') {
       self.zcache = {
         'index.html': ''
-      };
+      }
     }
 
     //  Local cache for static content.
-    self.zcache['index.html'] = fs.readFileSync('./views/index.html');
-    self.zcache['index.ejs'] = fs.readFileSync('./views/index.ejs');
-  };
-
+    self.zcache['index.html'] = fs.readFileSync('./views/index.html')
+    // self.zcache['index.ejs'] = fs.readFileSync('./views/index.ejs')
+  }
 
   /**
    *  Retrieve entry (content) from cache.
    *  @param {string} key  Key identifying content to retrieve from cache.
    */
-  self.cache_get = function(key) {
-    return self.zcache[key];
-  };
-
-
-
+  self.cache_get = function (key) {
+    return self.zcache[key]
+  }
 
   /**
    *  terminator === the termination handler
    *  Terminate server on receipt of the specified signal.
    *  @param {string} sig  Signal to terminate on.
    */
-  self.terminator = function(sig) {
-    if (typeof sig === "string") {
+  self.terminator = function (sig) {
+    if (typeof sig === 'string') {
       console.log('%s: Received %s - terminating sample app ...',
-        Date(Date.now()), sig);
-      process.exit(1);
+        Date(Date.now()), sig)
+      process.exit(1)
     }
-    console.log('%s: Node server stopped.', Date(Date.now()));
-  };
-
-
+    console.log('%s: Node server stopped.', Date(Date.now()))
+  }
   /**
    *  Setup termination handlers (for exit and a list of signals).
    */
-  self.setupTerminationHandlers = function() {
+  self.setupTerminationHandlers = function () {
     //  Process on exit and signals.
-    process.on('exit', function() {
-      self.terminator();
-    });
+    process.on('exit', function () {
+      self.terminator()
+    })
 
-    // Removed 'SIGPIPE' from the list - bugz 852598.
-    ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-      'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-    ].forEach(function(element, index, array) {
-      process.on(element, function() {
-        self.terminator(element);
-      });
-    });
-  };
+    const terminators = ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM']
 
+    terminators.forEach(function (element, index, array) {
+      process.on(element, function () {
+        self.terminator(element)
+      })
+    })
+  }
 
   /*  ================================================================  */
   /*  App server functions (main app logic here).                       */
@@ -159,34 +145,29 @@ const server = function() {
   /**
    *  Initializes the sample application.
    */
-  self.initialize = function() {
-    self.setupVariables();
-    self.populateCache();
-    self.setupTerminationHandlers();
+  self.initialize = function () {
+    self.setupVariables()
+    self.populateCache()
+    self.setupTerminationHandlers()
 
     // Create the express server and routes.
-    self.initializeServer();
-  };
-
+    self.initializeServer()
+  }
 
   /**
    *  Start the server (starts up the sample application).
    */
-  self.start = function() {
+  self.start = function () {
     //  Start the app on the specific interface (and port).
-    self.app.listen(self.port, self.ipaddress, function() {
-      console.log('%s: Node server started on %s:%d ...',
-        Date(Date.now()), self.ipaddress, self.port);
-    });
-  };
-
-}; /*  Sample Application.  */
-
-
-
+    self.app.listen(8080, function () {
+      console.log('%s: Node server started...',
+        Date(Date.now()))
+    })
+  }
+}
 /**
- *  main():  Main code.
+ *  launch function calls
  */
-let app = new server();
-app.initialize();
-app.start();
+let app = new Server()
+app.initialize()
+app.start()
